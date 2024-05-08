bb1ad38007ab0604446cecf8192745e2ea53d284
MacKenzie Clark <clark.mac99@gmail.com>
2024-05-08 17:57:37 +0000

    feat: First database querying

--- element.sh
+++ element.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# Check if no arguments were provided
-if [ "$#" -eq 0 ]; then
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
     echo "Please provide an element as an argument."
     exit 1
 fi
 
-# Placeholder for further implementation
-echo "Argument provided: $1"
+# Extract the argument
+element=$1
+
+# Query the database and output information about the element
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+query="SELECT * FROM elements WHERE atomic_number = '$element' OR symbol = '$element' OR name = '$element';"
+result=$($PSQL "$query")
+
+# Check if element is found
+if [ -z "$result" ]; then
+    echo "I could not find that element in the database."
+else
+    echo "$result"
+fi
